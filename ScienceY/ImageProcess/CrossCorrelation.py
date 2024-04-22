7e3c196cec27ee66ff1839db7a8f13d94bcf9e06
HuiyuZhao <huiyuzhao.tju@gmail.com>
2024-04-23 00:32:25 +0100

    XCORR modify

--- ScienceY/ImageProcess/CrossCorrelation.py
+++ ScienceY/ImageProcess/CrossCorrelation.py
@@ -5,14 +5,14 @@ Created on Mon Apr 22 15:37:42 2024
 @author: Huiyu
 """
 import numpy as np
-from scipy.signal import correlate2d
+from scipy.signal import correlate
 
 def CrossCorrelation(data2Da, data2Db):
     
     data2Da_mean_subtracted = data2Da - np.mean(data2Da)
     data2Db_mean_subtracted = data2Db - np.mean(data2Db)
     
-    cross_corr = correlate2d(data2Da_mean_subtracted, data2Db_mean_subtracted, mode = 'same', boundary='fill', )
+    cross_corr = correlate(data2Da_mean_subtracted, data2Db_mean_subtracted, mode = 'same')
     norm = np.sqrt((data2Da_mean_subtracted**2).sum() * (data2Db_mean_subtracted**2).sum())
     normalized_cross_corr = cross_corr / norm
     
