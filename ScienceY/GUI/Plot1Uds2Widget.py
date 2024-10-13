85d3af4c5423e24101b9ed8e1b7b8e2263cdca42
jhyan2018 <jhyan2018@gmail.com>
2024-10-13 22:30:55 +0100

    plotconfig framework

--- ScienceY/GUI/Plot1Uds2Widget.py
+++ ScienceY/GUI/Plot1Uds2Widget.py
@@ -24,6 +24,7 @@ User Modules
 """
 from ..RawDataProcess.UdsDataProcess import UdsDataStru
 from .general.NumberExpression import NumberExpression
+from .PlotConfig import PlotConfig
 
 """ *************************************** """
 """ DO NOT MODIFY THIS FILE"""
@@ -78,11 +79,14 @@ class Plot1Uds2Widget(QtWidgets.QWidget):
         #self.static_canvas.figure.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=None, hspace=None)
         #self.static_canvas.figure.patch.set_visible( False )
         
-        self.static_ax = self.static_canvas.figure.add_subplot(1,1,1)
-        #self.static_image = self.static_ax.imshow(np.ones((200,200)),interpolation = 'nearest',aspect = 'auto')
-    
+        static_ax = self.static_canvas.figure.add_subplot(1,1,1)
+        self.add_axis_item(static_ax, 1, 1)
+        
     def initNonUiMembers(self):
-        pass
+        self.uds_data = None
+        self.plot_config = None
+        self.axis_list=[]
+        self.line_list=[]
     
     def initUiLayout(self):
         
@@ -95,28 +99,74 @@ class Plot1Uds2Widget(QtWidgets.QWidget):
     def setCanvasWidgetSize(self, w, h):
         self.static_canvas.setFixedWidth(w)
         self.static_canvas.setFixedHeight(h)
+    
+    def add_axis_item(self, ax, nrow, ncol):
+        axis_item = {'ax':None,
+                     'subplot':None}
+        axis_item['ax'] = ax        
+        ax_idx = len(self.axis_list) 
+        axis_item['subplot'] = [nrow, ncol, ax_idx]        
+        self.axis_list.append(axis_item)
+    
+    def get_axis(self, ax_idx):
+        return self.axis_list[ax_idx]['ax']
+        
+    def add_line_item(self, line, ax_idx):
+        line_item = {'line':None,
+                     'ax_idx':None}
+        line_item['line'] = line
+        line_item['ax_idx'] = ax_idx
+        self.line_list.append(line_item)
+        
+    def get_line(self, line_idx):
+        return self.line_list[line_idx]['line']
+        
+    def plotLines(self):
+        self.get_axis(0) .clear()
+        
+        #
+        if len(self.uds_data.axis_value[0]) > 0:
+            x_axis = np.array(self.uds_data.axis_value[0])
+        else:
+            x_axis = range(self.uds_data.data.shape[1])
+        
+        #   
+        for i in range(self.uds_data.data.shape[0]):
+            line, = self.get_axis(0).plot(x_axis, self.uds_data.data[i,:])
+            self.add_line_item(line, 0)
+            
+        self.get_axis(0).figure.canvas.draw()
         
     def setUdsData(self, uds_data):
-        self.static_ax.clear()
-        
-        if len(uds_data.axis_value[0]) > 0:
-            x_axis = np.array(uds_data.axis_value[0])
+        if len(uds_data.data.shape) == 2:
+            self.line_list = []
+            self.uds_data = uds_data
+            self.plotLines()
+            if 'Plot_Config' in uds_data.hidden_info:
+                self.plot_config = uds_data.hidden_info['Plot_Config']
+            else:
+                self.plot_config = PlotConfig()
+                
+                # axis config
+                self.plot_config.add_config_axis()
+                config = {'xlabel':'Energy (V)',
+                          'ylabel':'Intensity (S)',
+                          'title':'dI/dV'}
+                self.plot_config.update_axis_config(0, config)
+                self.plot_config.apply_axis_config(self.get_axis(0) , 0)
+                
+                #self.get_axis(0).tick_params(axis='x', labelsize=18)
+                #self.get_axis(0).tick_params(axis='y', labelsize=18)
+                
+                # line config
+                for i in range(self.uds_data.data.shape[0]):
+                    self.plot_config.add_config_line()
+                    self.plot_config.apply_line_config(self.get_line(i) , i)
+                
         else:
-            x_axis = range(uds_data.data.shape[1])
-        for i in range(uds_data.data.shape[0]):
-            self.static_ax.plot(x_axis, uds_data.data[i,:] + i*5e-9, linewidth = 1.0, label='c'+str(i))
+            print('Unaccepted data dimension!')
+            return -1
         
-        self.static_ax.set_xlabel('Energy (V)', fontsize=20)
-        self.static_ax.set_ylabel('Intensity (S)', fontsize=20)
-        self.static_ax.set_title('dI/dV', fontsize=20)
+    """ Dynamic Fucntions"""
         
-        self.static_ax.tick_params(axis='x', labelsize=18)
-        self.static_ax.tick_params(axis='y', labelsize=18)
-        #self.static_ax.legend()
         
-        #self.static_ax.grid(True)
-        
-        #self.static_ax.set(xlim=(0,300),ylim=(0,300))
-        
-        self.static_ax.figure.canvas.draw()
-        self.static_canvas.flush_events()
